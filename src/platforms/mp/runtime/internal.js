835dbf76ad33e9731208672031abd81c7256a84c
fxy060608 <fxy060608@gmail.com>
2019-07-13 12:43:34 +0800

    fix(mp): handle error

--- src/platforms/mp/runtime/internal.js
+++ src/platforms/mp/runtime/internal.js
@@ -1,167 +1,171 @@
 /* @flow */
 
 import {
-    extend,
-    hyphenate,
-    isPlainObject
+  extend,
+  hyphenate,
+  isPlainObject
 } from 'shared/util'
 
 import {
-    pushTarget,
-    popTarget
+  pushTarget,
+  popTarget
 } from 'core/observer/dep'
 
 import {
-    initProvide,
-    initInjections
+  initProvide,
+  initInjections
 } from 'core/instance/inject'
 
 import {
-    toArray,
-    isObject,
-    invokeWithErrorHandling
+  toArray,
+  isObject,
+  invokeWithErrorHandling
 } from 'core/util/index'
 
 import {
-    renderClass
+  renderClass
 } from 'web/util/class'
 
 import {
-    normalizeStyleBinding
+  normalizeStyleBinding
 } from 'web/util/style'
 
 import {
-    nextTick
+  nextTick
 } from './next-tick'
 
 const MP_METHODS = ['createSelectorQuery', 'createIntersectionObserver', 'selectAllComponents', 'selectComponent']
 
 function getTarget(obj, path) {
-    const parts = path.split('.')
-    let key = parts[0]
-    if (key.indexOf('__$n') === 0) { //number index
-        key = parseInt(key.replace('__$n', ''))
-    }
-    if (parts.length === 1) {
-        return obj[key]
-    }
-    return getTarget(obj[key], parts.slice(1).join('.'))
+  const parts = path.split('.')
+  let key = parts[0]
+  if (key.indexOf('__$n') === 0) { //number index
+    key = parseInt(key.replace('__$n', ''))
+  }
+  if (parts.length === 1) {
+    return obj[key]
+  }
+  return getTarget(obj[key], parts.slice(1).join('.'))
 }
 
 export function internalMixin(Vue: Class<Component>) {
 
-    const oldEmit = Vue.prototype.$emit
+  Vue.config.errorHandler = function(err) {
+    console.error(err)
+  }
 
-    Vue.prototype.$emit = function(event: string): Component {
-        if (this.$scope && event) {
-            this.$scope['triggerEvent'](event, {
-                __args__: toArray(arguments, 1)
-            })
-        }
-        return oldEmit.apply(this, arguments)
+  const oldEmit = Vue.prototype.$emit
+
+  Vue.prototype.$emit = function(event: string): Component {
+    if (this.$scope && event) {
+      this.$scope['triggerEvent'](event, {
+        __args__: toArray(arguments, 1)
+      })
     }
+    return oldEmit.apply(this, arguments)
+  }
 
-    Vue.prototype.$nextTick = function(fn: Function) {
-        return nextTick(this, fn)
+  Vue.prototype.$nextTick = function(fn: Function) {
+    return nextTick(this, fn)
+  }
+
+  MP_METHODS.forEach(method => {
+    Vue.prototype[method] = function(args) {
+      if (this.$scope) {
+        return this.$scope[method](args)
+      }
     }
+  })
 
-    MP_METHODS.forEach(method => {
-        Vue.prototype[method] = function(args) {
-            if (this.$scope) {
-                return this.$scope[method](args)
-            }
-        }
-    })
+  Vue.prototype.__init_provide = initProvide
 
-    Vue.prototype.__init_provide = initProvide
+  Vue.prototype.__init_injections = initInjections
 
-    Vue.prototype.__init_injections = initInjections
-
-    Vue.prototype.__call_hook = function(hook, args) {
-        const vm = this
-        // #7573 disable dep collection when invoking lifecycle hooks
-        pushTarget()
-        const handlers = vm.$options[hook]
-        const info = `${hook} hook`
-        let ret
-        if (handlers) {
-            for (let i = 0, j = handlers.length; i < j; i++) {
-                ret = invokeWithErrorHandling(handlers[i], vm, args ? [args] : null, vm, info)
-            }
-        }
-        if (vm._hasHookEvent) {
-            vm.$emit('hook:' + hook)
-        }
-        popTarget()
-        return ret
+  Vue.prototype.__call_hook = function(hook, args) {
+    const vm = this
+    // #7573 disable dep collection when invoking lifecycle hooks
+    pushTarget()
+    const handlers = vm.$options[hook]
+    const info = `${hook} hook`
+    let ret
+    if (handlers) {
+      for (let i = 0, j = handlers.length; i < j; i++) {
+        ret = invokeWithErrorHandling(handlers[i], vm, args ? [args] : null, vm, info)
+      }
     }
-
-    Vue.prototype.__set_model = function(target, key, value, modifiers) {
-        if (Array.isArray(modifiers)) {
-            if (modifiers.indexOf('trim') !== -1) {
-                value = value.trim()
-            }
-            if (modifiers.indexOf('number') !== -1) {
-                value = this._n(value)
-            }
-        }
-        if (!target) {
-            target = this
-        }
-        target[key] = value
+    if (vm._hasHookEvent) {
+      vm.$emit('hook:' + hook)
     }
+    popTarget()
+    return ret
+  }
 
-    Vue.prototype.__set_sync = function(target, key, value) {
-        if (!target) {
-            target = this
-        }
-        target[key] = value
+  Vue.prototype.__set_model = function(target, key, value, modifiers) {
+    if (Array.isArray(modifiers)) {
+      if (modifiers.indexOf('trim') !== -1) {
+        value = value.trim()
+      }
+      if (modifiers.indexOf('number') !== -1) {
+        value = this._n(value)
+      }
     }
-
-    Vue.prototype.__get_orig = function(item) {
-        if (isPlainObject(item)) {
-            return item['$orig'] || item
-        }
-        return item
+    if (!target) {
+      target = this
     }
+    target[key] = value
+  }
 
-    Vue.prototype.__get_value = function(dataPath, target) {
-        return getTarget(target || this, dataPath)
+  Vue.prototype.__set_sync = function(target, key, value) {
+    if (!target) {
+      target = this
     }
+    target[key] = value
+  }
 
-
-    Vue.prototype.__get_class = function(dynamicClass, staticClass) {
-        return renderClass(staticClass, dynamicClass)
+  Vue.prototype.__get_orig = function(item) {
+    if (isPlainObject(item)) {
+      return item['$orig'] || item
     }
+    return item
+  }
 
-    Vue.prototype.__get_style = function(dynamicStyle, staticStyle) {
-        if (!dynamicStyle && !staticStyle) {
-            return ''
-        }
-        const dynamicStyleObj = normalizeStyleBinding(dynamicStyle)
-        const styleObj = staticStyle ? extend(staticStyle, dynamicStyleObj) : dynamicStyleObj
-        return Object.keys(styleObj).map(name => `${hyphenate(name)}:${styleObj[name]}`).join(';')
-    }
+  Vue.prototype.__get_value = function(dataPath, target) {
+    return getTarget(target || this, dataPath)
+  }
 
-    Vue.prototype.__map = function(val, iteratee) {
-        //TODO 暂不考虑 string,number
-        let ret, i, l, keys, key
-        if (Array.isArray(val)) {
-            ret = new Array(val.length)
-            for (i = 0, l = val.length; i < l; i++) {
-                ret[i] = iteratee(val[i], i)
-            }
-            return ret
-        } else if (isObject(val)) {
-            keys = Object.keys(val)
-            ret = Object.create(null)
-            for (i = 0, l = keys.length; i < l; i++) {
-                key = keys[i]
-                ret[key] = iteratee(val[key], key, i)
-            }
-            return ret
-        }
-        return []
+
+  Vue.prototype.__get_class = function(dynamicClass, staticClass) {
+    return renderClass(staticClass, dynamicClass)
+  }
+
+  Vue.prototype.__get_style = function(dynamicStyle, staticStyle) {
+    if (!dynamicStyle && !staticStyle) {
+      return ''
     }
+    const dynamicStyleObj = normalizeStyleBinding(dynamicStyle)
+    const styleObj = staticStyle ? extend(staticStyle, dynamicStyleObj) : dynamicStyleObj
+    return Object.keys(styleObj).map(name => `${hyphenate(name)}:${styleObj[name]}`).join(';')
+  }
+
+  Vue.prototype.__map = function(val, iteratee) {
+    //TODO 暂不考虑 string,number
+    let ret, i, l, keys, key
+    if (Array.isArray(val)) {
+      ret = new Array(val.length)
+      for (i = 0, l = val.length; i < l; i++) {
+        ret[i] = iteratee(val[i], i)
+      }
+      return ret
+    } else if (isObject(val)) {
+      keys = Object.keys(val)
+      ret = Object.create(null)
+      for (i = 0, l = keys.length; i < l; i++) {
+        key = keys[i]
+        ret[key] = iteratee(val[key], key, i)
+      }
+      return ret
+    }
+    return []
+  }
 
 }
--- src/platforms/mp/runtime/patch.js
+++ src/platforms/mp/runtime/patch.js
@@ -2,57 +2,64 @@
 import diff from './diff'
 
 import {
-    flushCallbacks
+  flushCallbacks
 } from './next-tick'
 
 function cloneWithData(vm) {
-    // 确保当前 vm 所有数据被同步
-    const dataKeys = [].concat(
-        Object.keys(vm._data || {}),
-        Object.keys(vm._computedWatchers || {}))
+  // 确保当前 vm 所有数据被同步
+  const ret = Object.create(null)
+  const dataKeys = [].concat(
+    Object.keys(vm._data || {}),
+    Object.keys(vm._computedWatchers || {}))
 
-    const ret = dataKeys.reduce(function(ret, key) {
-        ret[key] = vm[key]
-        return ret
-    }, Object.create(null))
-    //TODO 需要把无用数据处理掉，比如 list=>l0 则 list 需要移除，否则多传输一份数据
-    Object.assign(ret, vm.$mp.data || {})
-    if (
-        Array.isArray(vm.$options.behaviors) &&
-        vm.$options.behaviors.indexOf('uni://form-field') !== -1
-    ) { //form-field
-        ret['name'] = vm.name
-        ret['value'] = vm.value
-    }
-    return JSON.parse(JSON.stringify(ret))
+  dataKeys.reduce(function(ret, key) {
+    ret[key] = vm[key]
+    return ret
+  }, ret)
+  //TODO 需要把无用数据处理掉，比如 list=>l0 则 list 需要移除，否则多传输一份数据
+  Object.assign(ret, vm.$mp.data || {})
+  if (
+    Array.isArray(vm.$options.behaviors) &&
+    vm.$options.behaviors.indexOf('uni://form-field') !== -1
+  ) { //form-field
+    ret['name'] = vm.name
+    ret['value'] = vm.value
+  }
+
+  return JSON.parse(JSON.stringify(ret))
 }
 
 export const patch: Function = function(oldVnode, vnode) {
-    if (vnode === null) { //destroy
-        return
+  if (vnode === null) { //destroy
+    return
+  }
+  if (this.mpType === 'page' || this.mpType === 'component') {
+    const mpInstance = this.$scope
+    let data = Object.create(null)
+    try {
+      data = cloneWithData(this)
+    } catch (err) {
+      console.error(err)
     }
-    if (this.mpType === 'page' || this.mpType === 'component') {
-        const mpInstance = this.$scope
-        const data = cloneWithData(this)
-        data.__webviewId__ = mpInstance.data.__webviewId__
-        const mpData = Object.create(null)
-        Object.keys(data).forEach(key => { //仅同步 data 中有的数据
-            mpData[key] = mpInstance.data[key]
-        })
-        const diffData = diff(data, mpData)
-        if (Object.keys(diffData).length) {
-            if (process.env.VUE_APP_DEBUG) {
-                console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + this._uid +
-                    ']差量更新',
-                    JSON.stringify(diffData))
-            }
-            this.__next_tick_pending = true
-            mpInstance.setData(diffData, () => {
-                this.__next_tick_pending = false
-                flushCallbacks(this)
-            })
-        } else {
-            flushCallbacks(this)
-        }
+    data.__webviewId__ = mpInstance.data.__webviewId__
+    const mpData = Object.create(null)
+    Object.keys(data).forEach(key => { //仅同步 data 中有的数据
+      mpData[key] = mpInstance.data[key]
+    })
+    const diffData = diff(data, mpData)
+    if (Object.keys(diffData).length) {
+      if (process.env.VUE_APP_DEBUG) {
+        console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + this._uid +
+          ']差量更新',
+          JSON.stringify(diffData))
+      }
+      this.__next_tick_pending = true
+      mpInstance.setData(diffData, () => {
+        this.__next_tick_pending = false
+        flushCallbacks(this)
+      })
+    } else {
+      flushCallbacks(this)
     }
+  }
 }
