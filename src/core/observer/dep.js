c21233eb89fe65f1038aed701482263bf6e365a2
fxy060608 <fxy060608@gmail.com>
2020-07-20 15:46:20 +0800

    feat(mp): remove SharedObject

--- src/core/observer/dep.js
+++ src/core/observer/dep.js
@@ -16,13 +16,7 @@ export default class Dep {
   subs: Array<Watcher>;
 
   constructor () {
-    // fixed by xxxxxx (nvue vuex)
-    /* eslint-disable no-undef */
-    if(typeof SharedObject !== 'undefined'){
-      this.id = SharedObject.uid++
-    } else {
-      this.id = uid++
-    }
+    this.id = uid++
     this.subs = []
   }
 
@@ -60,7 +54,7 @@ export default class Dep {
 // can be evaluated at a time.
 // fixed by xxxxxx (nvue shared vuex)
 /* eslint-disable no-undef */
-Dep.SharedObject = typeof SharedObject !== 'undefined' ? SharedObject : {}
+Dep.SharedObject = {}
 Dep.SharedObject.target = null
 Dep.SharedObject.targetStack = []
 
